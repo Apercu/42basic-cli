1d3e8163cc9d0cc13ddb97d2acdaf7543c4fd50d
Aperçu <bgronon@gmail.com>
2015-07-05 16:00:06 +0200

    docs(auth): add small statement

--- scrap.js
+++ scrap.js
@@ -4,6 +4,7 @@ var request = require('request');
 
 module.exports = function (login) {
 
+  // Omg you're suck a h4xxxk3r you can read the auth token..
   request('https://api.intrav2.42.fr/users/' + login + '?token=233ad12d50787f39d03f684c2a0006fb4ba62443', function (err, response, body) {
     if (!err && response.statusCode === 200) {
       var data = JSON.parse(body);
